4f2d6fd4913d0c79621dcff25940bdf0935216e8
Cleber Rezende <contato.cleberrezende@gmail.com>
2026-05-10 01:03:26 -0300

    Add missing global usings for application currencies and domain value objects in integration and unit test files

--- tests/Wex.Purchases.UnitTests/GlobalUsings.cs
+++ tests/Wex.Purchases.UnitTests/GlobalUsings.cs
@@ -3,6 +3,7 @@ global using Moq;
 global using Moq.AutoMock;
 
 // Project
+global using Wex.Purchases.Application.Currencies;
 global using Wex.Purchases.Application.Requests;
 global using Wex.Purchases.Application.Results;
 global using Wex.Purchases.Application.Repositories;
@@ -10,6 +11,7 @@ global using Wex.Purchases.Application.Services;
 global using Wex.Purchases.Contracts.Requests;
 global using Wex.Purchases.Contracts.Results;
 global using Wex.Purchases.Domain.Entities;
+global using Wex.Purchases.Domain.ValueObjects;
 global using Wex.Purchases.Infrastructure.Treasury.Requests;
 global using Wex.Purchases.Infrastructure.Treasury.Services;
 global using Wex.Purchases.UnitTests.Fixtures;
